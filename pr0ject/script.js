7bc1f052a899d5b2b5d32dcd1452dde6a02961e3
poosy45 <45284889+poosy45@users.noreply.github.com>
2018-11-23 10:42:26 +0100

    Delete script.js

--- pr0ject/script.js
+++
@@ -1,8 +0,0 @@
-var message = ["Nami", "Heimerdonger", "Mordekaiser", "Bard", "Pyke", "Kalista", "Shaco", "Illaoi", "Lee son"],
-        pel = document.getElementById("text"),
-        idx = 0,
-    getNext = e => pel.innerText = e.target.id == "right" ?
-    message[idx = ++idx%message.length]
-  : message[idx = (message.length - (message.length - --idx)%message.length)%message.length];
-document.getElementById("right").onclick = getNext;
-document.getElementById("left").onclick = getNext;
